89949b8ce0592c370c2fe7d5e6a5c99b6b3eba87
marcosava <marcosava95@hotmail.com>
2022-04-03 09:46:17 -0500

    Re write the makeVector and the cachemean, to makeCacheMatrix and CacheSolve. The first functions, sets and gets the inverted matrix, and the later function looks for it, and if it has been caculated before, it prints it out.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Sets/gets the value of a matrix and its inverted form. Finally, stores it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- matrix()
+  set <- function(y) {
+    x <<- y
+    i <<- matrix()
+  }
+  get <- function() x
+  setinverted <- function(solve) i <<- solve
+  getinverted <- function() i
+  list(set = set, get = get,
+       setinverted = setinverted,
+       getinverted = getinverted)
 }
 
 
-## Write a short comment describing this function
+## Look for the inverted matrix, and if it has been calculated before, then it retrieves it; if it hasn't, it calculates it and stores it. Finally, it prints it out. 
 
 cacheSolve <- function(x, ...) {
+  i <- x$getinverted()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverted(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
